2fcb016911710ff8db4c88d0761ab6a39c58ac1b
giovandrofabio <giovandrofabiosantos@hotmail.com>
2022-06-07 23:01:58 -0300

    first commit

---
+++ src/main/java/br/com/treinaweb/adoteumpet/core/models/Pet.java
@@ -0,0 +1,35 @@
+package br.com.treinaweb.adoteumpet.core.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pet {
+    
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String historia;
+
+    @Column(nullable = false)
+    private String foto;
+    
+}
