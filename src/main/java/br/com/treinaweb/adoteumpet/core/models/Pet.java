6ed0c5e30d0187be3dc4e9f23d85b4c92c43b86c
giovandrofabio <giovandrofabiosantos@hotmail.com>
2022-06-09 00:15:32 -0300

    Second commit

--- src/main/java/br/com/treinaweb/adoteumpet/core/models/Pet.java
+++ src/main/java/br/com/treinaweb/adoteumpet/core/models/Pet.java
@@ -10,8 +10,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 @Data
+@Builder
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/br/com/treinaweb/adoteumpet/core/repositories/PetRepository.java
+++ src/main/java/br/com/treinaweb/adoteumpet/core/repositories/PetRepository.java
@@ -1,20 +1,21 @@
 package br.com.treinaweb.adoteumpet.core.repositories;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.treinaweb.adoteumpet.core.exceptions.PetNotFoundException;
 import br.com.treinaweb.adoteumpet.core.models.Pet;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
     
     default Pet findByIdOrElseThrow(Long id){
-        var petOptional = findById(id);
+        // var petOptional = findById(id);
 
-        if(petOptional.isPresent()){
-            return petOptional.get();
-        }
+        // if(petOptional.isPresent()){
+        //     return petOptional.get();
+        // }
         
-        throw new EntityNotFoundException("Pet não encontrado");
+        // throw new PetNotFoundException();
+        return findById(id)
+             .orElseThrow(PetNotFoundException::new);
     }
 }
